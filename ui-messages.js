76eb4a9314966434de387b171d18d96dd39330a5
tilwinjoy <tilwinjoy@gmail.com>
2016-02-23 16:07:35 +0530

    Create ui-messages.js

---
+++ ui-messages.js
@@ -0,0 +1,31 @@
+/**
+ * Simple ng-messages like implementation for angular < 1.3
+ *
+ * @author Tilwin Joy <tilwinjoy@gmail.com>
+ */
+ 
+angular.module('uiMessages', []).directive('uiMessages', function() {
+
+    return {
+        restrict: 'EA',
+        link: function(scope, element, attrs) {
+            // hide all message elements
+            var messageElements = element.find('ui-message,[ui-message]').css('display', 'none');
+
+            // watch when messages object change - change display state of the elements
+            scope.$watchCollection(attrs.uiMessages || attrs['for'], function(messages) {
+                var oneElementAlreadyShowed = false;
+                angular.forEach(messageElements, function(messageElement) {
+                    messageElement = angular.element(messageElement);
+                    var message = messageElement.attr('uiMessage') || messageElement.attr('when');
+                    if (!oneElementAlreadyShowed && messages[message] && messages[message] === true) {
+                        messageElement.css('display', 'block');
+                        oneElementAlreadyShowed = true;
+                    } else {
+                        messageElement.css('display', 'none');
+                    }
+                });
+            });
+        }
+    };
+});
